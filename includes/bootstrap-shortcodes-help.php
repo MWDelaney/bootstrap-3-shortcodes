2517dcfed39eaf90dd5733d40974a688c860028f
Michael <foolsrun@swingmonkey.com>
2013-09-17 21:12:16 -0400

    Open external links in new tab/window

--- includes/bootstrap-shortcodes-help.php
+++ includes/bootstrap-shortcodes-help.php
@@ -56,6 +56,7 @@ $html = MarkdownExtra::defaultTransform($text);
         <div class="container">
 		<?php
 			# Put HTML content in the document
+            $html = preg_replace('/(<a href="http:[^"]+")>/is','\\1 target="_blank">',$html);
             $html = str_replace('<table>', '<table class="table table-striped">', $html);
             $html = preg_replace_callback("#<(h[1-6])>(.*?)</\\1>#", "retitle", $html);
             $html = str_replace('</pre>', '</pre><p><button class="btn btn-primary btn-sm insert-code">Insert Example <i class="glyphicon glyphicon-share-alt"></i></button></p>', $html);
