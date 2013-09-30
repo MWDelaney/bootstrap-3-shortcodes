6a37a61c1713796f720f4c879c08e613280d8c1a
Filip Stefansson <filip.stefansson@gmail.com>
2013-09-30 14:07:55 +0200

    Updated plugin version to 1.0.

--- bootstrap-shortcodes.php
+++ bootstrap-shortcodes.php
@@ -3,7 +3,7 @@
 Plugin Name: Bootstrap 3 Shortcodes
 Plugin URI: http://wp-snippets.com/freebies/bootstrap-shortcodes or https://github.com/filipstefansson/bootstrap-shortcodes
 Description: The plugin adds a shortcodes for all Bootstrap elements.
-Version: 0.1
+Version: 1.0
 Author: Filip Stefansson and Michael W. Delaney
 Author URI: 
 License: GPL2
