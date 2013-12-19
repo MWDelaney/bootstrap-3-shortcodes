0b3fa590cabdb65d29ca8722f65a06bee45aa3f2
FoolsRun <michael@swingmonkey.com>
2013-12-19 13:45:51 -0500

    New admin button icons and styles for WP 3.8

--- includes/actions-filters.php
+++ includes/actions-filters.php
@@ -1,4 +1,5 @@
 <?php
+    
 add_filter('the_content', 'bs_fix_shortcodes');
 
 // Create a Media Button for the help file
@@ -10,7 +11,7 @@ add_action('media_buttons_context', 'add_bootstrap_button');
 function add_bootstrap_button($context) {
   
   //path to my icon
-  $img = BS_SHORTCODES_URL . 'images/bootstrap-logo.png';
+  $img = BS_SHORTCODES_URL . 'images/Twitter_Boostrap_logo.svg';
   
   //the id of the container I want to show in the popup
   $popup_url = BS_SHORTCODES_URL . 'bootstrap-shortcodes-help.php';
@@ -19,9 +20,9 @@ function add_bootstrap_button($context) {
   $title = 'Bootstrap Shortcodes Help';
 
   //append the icon
-  $context .= "<a class='thickbox' title='{$title}'
-    href='{$popup_url}?TB_iframe=true&width=400'>
-    <img src='{$img}' /></a>";
+  $context .= "<a title='{$title}'
+    href='{$popup_url}?TB_iframe=true&width=450' class='thickbox button add_media' style='padding-left: 0px; padding-right: 0px;' title='Bootstrap Shortcodes Help'>
+    <img src='{$img}' style='height: 20px; position: relative; top: -2px;'></a>";
   
   return $context;
 }
