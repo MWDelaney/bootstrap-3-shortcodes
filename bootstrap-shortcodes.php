1461eea4211520b1aa8cadd68b6bc1937768e8f1
Michael <foolsrun@swingmonkey.com>
2014-01-16 21:56:55 -0500

    Don't load help styles outside admin

--- bootstrap-shortcodes.php
+++ bootstrap-shortcodes.php
@@ -34,13 +34,13 @@ require_once(dirname(__FILE__) . '/includes/actions-filters.php');
 function bootsrap_shortcodes_scripts()  { 
 
   // Bootstrap tooltip js
-  wp_enqueue_script( 'bootsrap-shortcodes-tooltip', BS_SHORTCODES_URL . 'js/bootstrap-shortcodes-tooltip.js', array( 'jquery' ), false, true );
+  wp_enqueue_script( 'bootstrap-shortcodes-tooltip', BS_SHORTCODES_URL . 'js/bootstrap-shortcodes-tooltip.js', array( 'jquery' ), false, true );
   
   // Bootstrap popover js
-  wp_enqueue_script( 'bootsrap-shortcodes-popover', BS_SHORTCODES_URL . 'js/bootstrap-shortcodes-popover.js', array( 'jquery' ), false, true );
+  wp_enqueue_script( 'bootstrap-shortcodes-popover', BS_SHORTCODES_URL . 'js/bootstrap-shortcodes-popover.js', array( 'jquery' ), false, true );
 
 }
-add_action( 'wp_enqueue_scripts', 'bootsrap_shortcodes_scripts' ); // Register this fxn and allow Wordpress to call it automatcally in the header
+add_action( 'wp_enqueue_scripts', 'bootsrap_shortcodes_scripts', 9999 ); // Register this fxn and allow Wordpress to call it automatcally in the header
 
 // Begin Shortcodes
 class BoostrapShortcodes {
--- includes/actions-filters.php
+++ includes/actions-filters.php
@@ -1,6 +1,9 @@
 <?php
+function bootstrap_shortcodes_help_styles() {
 	wp_register_style( 'bootstrap-shortcodes-help', plugins_url( 'bootstrap-3-shortcodes/includes/help/css/bootstrap-shortcodes-help.css' ) );
 	wp_enqueue_style( 'bootstrap-shortcodes-help' );
+}
+add_action( 'admin_enqueue_scripts', 'bootstrap_shortcodes_help_styles' );
 
 add_filter('the_content', 'bs_fix_shortcodes');
 
