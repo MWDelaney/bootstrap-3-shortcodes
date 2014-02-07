0cb2157103c19b2b609aeb869f3f0e6fde00d227
FoolsRun <michael@swingmonkey.com>
2014-02-07 10:08:02 -0500

    Attempted fix of file paths on Windows

--- includes/defaults.php
+++ includes/defaults.php
@@ -1,15 +1,15 @@
 <?php
 
 // Windows-proof constants: replace backward by forward slashes - thanks to: https://github.com/peterbouwmeester
-$fslashed_dir = trailingslashit(str_replace('\\','/', dirname(__FILE__)));
-$fslashed_abs = trailingslashit(str_replace('\\','/', ABSPATH));
+//$fslashed_dir = trailingslashit(str_replace('\\','/', dirname(__FILE__)));
+//$fslashed_abs = trailingslashit(str_replace('\\','/', ABSPATH));
 
 if(!defined('BS_SHORTCODES_DIR')) {
-    define('BS_SHORTCODES_DIR', $fslashed_dir);
+    define('BS_SHORTCODES_DIR', plugin_dir_path( __FILE__ ));
 }
 
 if(!defined('BS_SHORTCODES_URL')) {
-    define('BS_SHORTCODES_URL', site_url(str_replace($fslashed_abs, '', $fslashed_dir)));
+    define('BS_SHORTCODES_URL', plugin_dir_url( __FILE__ ));
 }
 
 ?>
