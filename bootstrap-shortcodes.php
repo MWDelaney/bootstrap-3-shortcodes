fb2703100096a9e051ab8c9296317ba93e91e94b
MWDelaney <foolsrun@swingmonkey.com>
2014-11-25 14:57:47 -0500

    Add support for offsets, pulls, and pushes of 0 in [column]

--- bootstrap-shortcodes.php
+++ bootstrap-shortcodes.php
@@ -670,23 +670,23 @@ class BoostrapShortcodes {
 	), $atts );
 
     $class  = '';
-    $class .= ( $atts['lg'] )			  ? ' col-lg-' . $atts['lg'] : '';
-    $class .= ( $atts['md'] )             ? ' col-md-' . $atts['md'] : '';
-    $class .= ( $atts['sm'] )             ? ' col-sm-' . $atts['sm'] : '';
-    $class .= ( $atts['xs'] )             ? ' col-xs-' . $atts['xs'] : '';
-    $class .= ( $atts['offset_lg'] )      ? ' col-lg-offset-' . $atts['offset_lg'] : '';
-    $class .= ( $atts['offset_md'] )      ? ' col-md-offset-' . $atts['offset_md'] : '';
-    $class .= ( $atts['offset_sm'] )      ? ' col-sm-offset-' . $atts['offset_sm'] : '';
-    $class .= ( $atts['offset_xs'] )      ? ' col-xs-offset-' . $atts['offset_xs'] : '';
-    $class .= ( $atts['pull_lg'] )        ? ' col-lg-pull-' . $atts['pull_lg'] : '';
-    $class .= ( $atts['pull_md'] )        ? ' col-md-pull-' . $atts['pull_md'] : '';
-    $class .= ( $atts['pull_sm'] )        ? ' col-sm-pull-' . $atts['pull_sm'] : '';
-    $class .= ( $atts['pull_xs'] )        ? ' col-xs-pull-' . $atts['pull_xs'] : '';
-    $class .= ( $atts['push_lg'] )        ? ' col-lg-push-' . $atts['push_lg'] : '';
-    $class .= ( $atts['push_md'] )        ? ' col-md-push-' . $atts['push_md'] : '';
-    $class .= ( $atts['push_sm'] )        ? ' col-sm-push-' . $atts['push_sm'] : '';
-    $class .= ( $atts['push_xs'] )        ? ' col-xs-push-' . $atts['push_xs'] : '';
-    $class .= ( $atts['xclass'] )   ? ' ' . $atts['xclass'] : '';
+    $class .= ( $atts['lg'] )			                                ? ' col-lg-' . $atts['lg'] : '';
+    $class .= ( $atts['md'] )                                           ? ' col-md-' . $atts['md'] : '';
+    $class .= ( $atts['sm'] )                                           ? ' col-sm-' . $atts['sm'] : '';
+    $class .= ( $atts['xs'] )                                           ? ' col-xs-' . $atts['xs'] : '';
+    $class .= ( $atts['offset_lg'] || $atts['offset_lg'] === "0" )      ? ' col-lg-offset-' . $atts['offset_lg'] : '';
+    $class .= ( $atts['offset_md'] || $atts['offset_md'] === "0" )      ? ' col-md-offset-' . $atts['offset_md'] : '';
+    $class .= ( $atts['offset_sm'] || $atts['offset_sm'] === "0" )      ? ' col-sm-offset-' . $atts['offset_sm'] : '';
+    $class .= ( $atts['offset_xs'] || $atts['offset_xs'] === "0" )      ? ' col-xs-offset-' . $atts['offset_xs'] : '';
+    $class .= ( $atts['pull_lg'] || $atts['pull_lg'] === "0" )          ? ' col-lg-pull-' . $atts['pull_lg'] : '';
+    $class .= ( $atts['pull_md'] || $atts['pull_md'] === "0" )          ? ' col-md-pull-' . $atts['pull_md'] : '';
+    $class .= ( $atts['pull_sm'] || $atts['pull_sm'] === "0" )          ? ' col-sm-pull-' . $atts['pull_sm'] : '';
+    $class .= ( $atts['pull_xs'] || $atts['pull_xs'] === "0" )          ? ' col-xs-pull-' . $atts['pull_xs'] : '';
+    $class .= ( $atts['push_lg'] || $atts['push_lg'] === "0" )          ? ' col-lg-push-' . $atts['push_lg'] : '';
+    $class .= ( $atts['push_md'] || $atts['push_md'] === "0" )          ? ' col-md-push-' . $atts['push_md'] : '';
+    $class .= ( $atts['push_sm'] || $atts['push_sm'] === "0" )          ? ' col-sm-push-' . $atts['push_sm'] : '';
+    $class .= ( $atts['push_xs'] || $atts['push_xs'] === "0" )          ? ' col-xs-push-' . $atts['push_xs'] : '';
+    $class .= ( $atts['xclass'] )                                       ? ' ' . $atts['xclass'] : '';
       
     $data_props = $this->parse_data_attributes( $atts['data'] );
       
