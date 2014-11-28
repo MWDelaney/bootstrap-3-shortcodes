50033ef3b2b5f36d0ff8952c0b63abad48849e62
MWDelaney <foolsrun@swingmonkey.com>
2014-11-28 09:41:21 -0500

    Add support for responsive tables in [table-wrap]

--- bootstrap-shortcodes.php
+++ bootstrap-shortcodes.php
@@ -678,14 +678,14 @@ class BoostrapShortcodes {
     $class .= ( $atts['offset_md'] || $atts['offset_md'] === "0" )      ? ' col-md-offset-' . $atts['offset_md'] : '';
     $class .= ( $atts['offset_sm'] || $atts['offset_sm'] === "0" )      ? ' col-sm-offset-' . $atts['offset_sm'] : '';
     $class .= ( $atts['offset_xs'] || $atts['offset_xs'] === "0" )      ? ' col-xs-offset-' . $atts['offset_xs'] : '';
-    $class .= ( $atts['pull_lg'] || $atts['pull_lg'] === "0" )          ? ' col-lg-pull-' . $atts['pull_lg'] : '';
-    $class .= ( $atts['pull_md'] || $atts['pull_md'] === "0" )          ? ' col-md-pull-' . $atts['pull_md'] : '';
-    $class .= ( $atts['pull_sm'] || $atts['pull_sm'] === "0" )          ? ' col-sm-pull-' . $atts['pull_sm'] : '';
-    $class .= ( $atts['pull_xs'] || $atts['pull_xs'] === "0" )          ? ' col-xs-pull-' . $atts['pull_xs'] : '';
-    $class .= ( $atts['push_lg'] || $atts['push_lg'] === "0" )          ? ' col-lg-push-' . $atts['push_lg'] : '';
-    $class .= ( $atts['push_md'] || $atts['push_md'] === "0" )          ? ' col-md-push-' . $atts['push_md'] : '';
-    $class .= ( $atts['push_sm'] || $atts['push_sm'] === "0" )          ? ' col-sm-push-' . $atts['push_sm'] : '';
-    $class .= ( $atts['push_xs'] || $atts['push_xs'] === "0" )          ? ' col-xs-push-' . $atts['push_xs'] : '';
+    $class .= ( $atts['pull_lg']   || $atts['pull_lg'] === "0" )        ? ' col-lg-pull-' . $atts['pull_lg'] : '';
+    $class .= ( $atts['pull_md']   || $atts['pull_md'] === "0" )        ? ' col-md-pull-' . $atts['pull_md'] : '';
+    $class .= ( $atts['pull_sm']   || $atts['pull_sm'] === "0" )        ? ' col-sm-pull-' . $atts['pull_sm'] : '';
+    $class .= ( $atts['pull_xs']   || $atts['pull_xs'] === "0" )        ? ' col-xs-pull-' . $atts['pull_xs'] : '';
+    $class .= ( $atts['push_lg']   || $atts['push_lg'] === "0" )        ? ' col-lg-push-' . $atts['push_lg'] : '';
+    $class .= ( $atts['push_md']   || $atts['push_md'] === "0" )        ? ' col-md-push-' . $atts['push_md'] : '';
+    $class .= ( $atts['push_sm']   || $atts['push_sm'] === "0" )        ? ' col-sm-push-' . $atts['push_sm'] : '';
+    $class .= ( $atts['push_xs']   || $atts['push_xs'] === "0" )        ? ' col-xs-push-' . $atts['push_xs'] : '';
     $class .= ( $atts['xclass'] )                                       ? ' ' . $atts['xclass'] : '';
       
     $data_props = $this->parse_data_attributes( $atts['data'] );
@@ -1014,20 +1014,21 @@ class BoostrapShortcodes {
   function bs_table_wrap( $atts, $content = null ) {
 
 	$atts = shortcode_atts( array(
-      'bordered'  => false,
-      'striped'   => false,
-      'hover'     => false,
-      'condensed' => false,
-      'xclass'    => false,
-      'data'      => false
+      'bordered'   => false,
+      'striped'    => false,
+      'hover'      => false,
+      'condensed'  => false,
+      'responsive' => false,
+      'xclass'     => false,
+      'data'       => false
 	), $atts );
 
     $class  = 'table';
-    $class .= ( $atts['bordered']  == 'true' )     ? ' table-bordered' : '';
-    $class .= ( $atts['striped']   == 'true' )      ? ' table-striped' : '';
-    $class .= ( $atts['hover']     == 'true' )      ? ' table-hover' : '';
+    $class .= ( $atts['bordered']  == 'true' )    ? ' table-bordered' : '';
+    $class .= ( $atts['striped']   == 'true' )    ? ' table-striped' : '';
+    $class .= ( $atts['hover']     == 'true' )    ? ' table-hover' : '';
     $class .= ( $atts['condensed'] == 'true' )    ? ' table-condensed' : '';
-    $class .= ( $atts['xclass'] )       ? ' ' . $atts['xclass'] : '';
+    $class .= ( $atts['xclass'] )                 ? ' ' . $atts['xclass'] : '';
       
     $return = '';
       
@@ -1035,6 +1036,7 @@ class BoostrapShortcodes {
     $content = do_shortcode($content);
 
     $return .= $this->scrape_dom_element($tag, $content, $class, '', $atts['data']);
+    $return = ( $atts['responsive'] ) ? '<div class="table-responsive">' . $return . '</div>' : $return;
     return $return;
   }
     
